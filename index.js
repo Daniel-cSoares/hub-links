ace201b9d00ad594cc9a48a35171fa2884cb5c1a
Daniel-cSoares <danielcsoares17@gmail.com>
2023-09-27 01:02:15 -0300

    fix

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function toggleMode(){
     const profile = document.querySelector("#profile img")
     if (html.classList.contains("light")){
         html.classList.remove("light")
-        profile.setAttribute("src", "/assets/my_avatar.png")
+        profile.setAttribute("src", "./assets/my_avatar.png")
     }
     else{
         html.classList.add("light")
-        profile.setAttribute("src", "/assets/my_avatar_light.png")
+        profile.setAttribute("src", "./assets/my_avatar_light.png")
     }
     // Faz a mesma coisa
     // html.classList.toggle("light")
