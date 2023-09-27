25a258360dc13545c8b9fccd752c1081e7449516
Daniel-cSoares <danielcsoares17@gmail.com>
2023-09-26 21:22:13 -0300

    final

---
+++ index.js
@@ -0,0 +1,14 @@
+function toggleMode(){
+    const html = document.documentElement
+    const profile = document.querySelector("#profile img")
+    if (html.classList.contains("light")){
+        html.classList.remove("light")
+        profile.setAttribute("src", "/assets/my_avatar.png")
+    }
+    else{
+        html.classList.add("light")
+        profile.setAttribute("src", "/assets/my_avatar_light.png")
+    }
+    // Faz a mesma coisa
+    // html.classList.toggle("light")
+}
